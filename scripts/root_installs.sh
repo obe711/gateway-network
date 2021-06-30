35bdabf029f69abe2dc8ea2671658757c20b5b8a
Obediah Klopfenstein <obe711@gmail.com>
2021-06-30 11:29:45 -0500

    added cockpit

--- scripts/root_installs.sh
+++ scripts/root_installs.sh
@@ -5,6 +5,7 @@ apt install gnupg
 apt install sysstat
 apt install glances
 apt install msr-tools
+apt install cockpit
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 apt update -y
